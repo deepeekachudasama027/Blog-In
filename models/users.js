133ac6845265558493e2787a02d5cb7bf7c65d32
027DeepeekaChudasama <deepeekachudasama@gmail.com>
2021-06-22 15:41:24 +0530

    Updated

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 var db = require("../controller/db");
 
 db.query(
-  "CREATE TABLE IF NOT EXISTS users (user_name varchar(255) NOT NULL,ID int AUTO_INCREMENT PRIMARY KEY  NOT NULL, email varchar(255) NOT NULL Unique, password varchar(255) NOT NULL)",
+  "CREATE TABLE IF NOT EXISTS users (username varchar(255) NOT NULL,id int AUTO_INCREMENT PRIMARY KEY  NOT NULL, email varchar(255) NOT NULL Unique, password varchar(255) NOT NULL)",
   (err, result) => {
     if (err) console.log(err);
   }
