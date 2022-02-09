e5fc59369d38d6decc30bb0ec017157762d7ee4b
Deepeeka Chudasama <deepeekachudasama@gmail.com>
2022-02-09 13:44:14 +0530

    Update db.js

--- controller/db.js
+++ controller/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "root",
+  user: "",
+  password: "",
 });
 
 db.connect(function (err) {
